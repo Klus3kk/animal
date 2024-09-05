35ea4aee93f420127ade379573587e9d6365e689
Klus3kk <lukasz.bielaszewski@onet.pl>
2024-09-05 18:00:20 +0200

    Animal v.0.1.3

--- animal.go
+++ animal.go
@@ -25,6 +25,38 @@ func (e Error) asString() string {
 	return result
 }
 
+// IllegalCharError struct inheriting from Error
+type IllegalCharError struct {
+	Error
+}
+
+func NewIllegalCharError(posStart, posEnd *Position, details string) *IllegalCharError {
+	return &IllegalCharError{
+		Error: Error{
+			PosStart:  posStart,
+			PosEnd:    posEnd,
+			ErrorName: "Illegal Character",
+			Details:   details,
+		},
+	}
+}
+
+// InvalidSyntaxError struct inheriting from Error
+type InvalidSyntaxError struct {
+	Error
+}
+
+func NewInvalidSyntaxError(posStart, posEnd *Position, details string) *InvalidSyntaxError {
+	return &InvalidSyntaxError{
+		Error: Error{
+			PosStart:  posStart,
+			PosEnd:    posEnd,
+			ErrorName: "Invalid Syntax",
+			Details:   details,
+		},
+	}
+}
+
 // POSITION //
 
 type Position struct {
@@ -84,8 +116,10 @@ const (
 
 // Token represents a token with its type and value
 type Token struct {
-	Type  TokenType
-	Value string
+	Type      TokenType
+	Value     string
+	Pos_Start *Position
+	Pos_End   *Position
 }
 
 // String returns a string representation of the Token
@@ -140,44 +174,49 @@ func (l *Lexer) make_tokens() ([]Token, error) {
 		if l.CurrentChar == ' ' || l.CurrentChar == '\t' {
 			l.advance()
 		} else if strings.IndexByte(DIGITS, l.CurrentChar) != -1 {
-			tokens = append(tokens, l.make_number())
+			tokens = append(tokens, l.make_number()) // Tokenize number
 		} else if l.peek(4) == "meow" {
-			tokens = append(tokens, Token{Type: TT_PLUS, Value: "PLUS"})
+			posStart := l.Pos.copy() // Copy the current position as the token's start
 			l.advanceBy(4)
+			posEnd := l.Pos.copy() // After advancing, copy for the token's end
+			tokens = append(tokens, Token{Type: TT_PLUS, Value: "PLUS", Pos_Start: posStart, Pos_End: posEnd})
 		} else if l.peek(4) == "woof" {
-			tokens = append(tokens, Token{Type: TT_MINUS, Value: "MINUS"})
+			posStart := l.Pos.copy()
 			l.advanceBy(4)
+			posEnd := l.Pos.copy()
+			tokens = append(tokens, Token{Type: TT_MINUS, Value: "MINUS", Pos_Start: posStart, Pos_End: posEnd})
 		} else if l.peek(3) == "moo" {
-			tokens = append(tokens, Token{Type: TT_MUL, Value: "MUL"})
+			posStart := l.Pos.copy()
 			l.advanceBy(3)
+			posEnd := l.Pos.copy()
+			tokens = append(tokens, Token{Type: TT_MUL, Value: "MUL", Pos_Start: posStart, Pos_End: posEnd})
 		} else if l.peek(5) == "drone" {
-			tokens = append(tokens, Token{Type: TT_DIV, Value: "DIV"})
+			posStart := l.Pos.copy()
 			l.advanceBy(5)
+			posEnd := l.Pos.copy()
+			tokens = append(tokens, Token{Type: TT_DIV, Value: "DIV", Pos_Start: posStart, Pos_End: posEnd})
 		} else if l.peek(6) == "squeak" {
-			tokens = append(tokens, Token{Type: TT_MOD, Value: "MOD"})
+			posStart := l.Pos.copy()
 			l.advanceBy(6)
+			posEnd := l.Pos.copy()
+			tokens = append(tokens, Token{Type: TT_MOD, Value: "MOD", Pos_Start: posStart, Pos_End: posEnd})
 		} else if l.peek(4) == "soar" {
-			tokens = append(tokens, Token{Type: TT_EXP, Value: "EXP"})
+			posStart := l.Pos.copy()
 			l.advanceBy(4)
+			posEnd := l.Pos.copy()
+			tokens = append(tokens, Token{Type: TT_EXP, Value: "EXP", Pos_Start: posStart, Pos_End: posEnd})
 		} else if l.CurrentChar == '(' {
-			tokens = append(tokens, Token{Type: TT_LROUNDBR, Value: string(l.CurrentChar)})
+			posStart := l.Pos.copy()
 			l.advance()
+			posEnd := l.Pos.copy()
+			tokens = append(tokens, Token{Type: TT_LROUNDBR, Value: string(l.CurrentChar), Pos_Start: posStart, Pos_End: posEnd})
 		} else if l.CurrentChar == ')' {
-			tokens = append(tokens, Token{Type: TT_RROUNDBR, Value: string(l.CurrentChar)})
+			posStart := l.Pos.copy()
 			l.advance()
-		} else if l.CurrentChar == '[' {
-			tokens = append(tokens, Token{Type: TT_LSQRBR, Value: string(l.CurrentChar)})
-			l.advance()
-		} else if l.CurrentChar == ']' {
-			tokens = append(tokens, Token{Type: TT_RSQRBR, Value: string(l.CurrentChar)})
-			l.advance()
-		} else if l.CurrentChar == '{' {
-			tokens = append(tokens, Token{Type: TT_LCURLBR, Value: string(l.CurrentChar)})
-			l.advance()
-		} else if l.CurrentChar == '}' {
-			tokens = append(tokens, Token{Type: TT_RCURLBR, Value: string(l.CurrentChar)})
-			l.advance()
+			posEnd := l.Pos.copy()
+			tokens = append(tokens, Token{Type: TT_RROUNDBR, Value: string(l.CurrentChar), Pos_Start: posStart, Pos_End: posEnd})
 		} else {
+			// Handling illegal characters
 			posStart := l.Pos.copy()
 			char := string(l.CurrentChar)
 			l.advance()
@@ -185,6 +224,7 @@ func (l *Lexer) make_tokens() ([]Token, error) {
 			break
 		}
 	}
+	tokens = append(tokens, Token{Type: TT_EOF, Value: "EOF", Pos_Start: l.Pos.copy(), Pos_End: l.Pos.copy()})
 	return tokens, err
 }
 
@@ -197,6 +237,12 @@ func (l *Lexer) advanceBy(count int) {
 func (l *Lexer) make_number() Token {
 	numStr := ""
 	dotCount := 0
+	Pos_Start := l.Pos.copy()
+
+	if l.CurrentChar == '-' {
+		numStr += "-"
+		l.advance()
+	}
 
 	for l.CurrentChar != 0 && strings.ContainsAny(string(l.CurrentChar), DIGITS+".") {
 		if l.CurrentChar == '.' {
@@ -213,15 +259,15 @@ func (l *Lexer) make_number() Token {
 
 	if dotCount == 0 {
 		intValue, _ := strconv.Atoi(numStr) // Convert numStr to int
-		return Token{Type: TT_INT, Value: strconv.Itoa(intValue)}
+		return Token{Type: TT_INT, Value: strconv.Itoa(intValue), Pos_Start: Pos_Start, Pos_End: l.Pos}
 	} else {
 		floatValue, _ := strconv.ParseFloat(numStr, 64) // Convert numStr to float64
-		return Token{Type: TT_FLOAT, Value: strconv.FormatFloat(floatValue, 'f', -1, 64)}
+		return Token{Type: TT_FLOAT, Value: strconv.FormatFloat(floatValue, 'f', -1, 64), Pos_Start: Pos_Start, Pos_End: l.Pos}
 	}
 }
 
 // NODES //
-// __init__ (self, tok)
+// __init__ (self, Tok)
 
 type NumberNode struct {
 	Tok Token
@@ -237,8 +283,44 @@ type BinOpNode struct {
 	Right_Node interface{}
 }
 
-func (b BinOpNode) String() string {
+func (b BinOpNode) String() string { // __repr__
 	return fmt.Sprintf("(%s %s %s)", b.Left_Node, b.Op_Tok.Value, b.Right_Node)
+}
+
+type UnaryOpNode struct {
+	Op_Tok Token
+	Node   interface{}
+}
+
+func (u UnaryOpNode) String() string { // __repr__
+	return fmt.Sprintf("(%s %s)", u.Op_Tok.Value, u.Node)
+}
+
+// PARSE RESULT //
+
+type ParseResult struct {
+	Error string
+	Node  interface{}
+}
+
+// Registers a ParseResult, storing its error and node
+func (pr *ParseResult) register(res *ParseResult) interface{} {
+	if res.Error != "" {
+		pr.Error = res.Error
+	}
+	return res.Node
+}
+
+// Registers a successful parsing result
+func (pr *ParseResult) success(node interface{}) *ParseResult {
+	pr.Node = node
+	return pr
+}
+
+// Registers a failure with an error message
+func (pr *ParseResult) failure(error string) *ParseResult {
+	pr.Error = error
+	return pr
 }
 
 // PARSER //
@@ -268,51 +350,80 @@ func (p *Parser) advance() Token {
 
 ////////////
 
-func (p *Parser) parse() interface{} {
+func (p *Parser) parse() *ParseResult {
 	res := p.expr()
+	if res.Error == "" && p.Current_Tok.Type != TT_EOF {
+		return res.failure(NewInvalidSyntaxError(
+			p.Current_Tok.Pos_Start, p.Current_Tok.Pos_End,
+			"Expected '+', '-', '*', '/'",
+		).asString())
+	}
 	return res
 }
 
-func (p *Parser) factor() interface{} {
+func (p *Parser) factor() *ParseResult {
+	res := &ParseResult{}
 	tok := p.Current_Tok
 
 	if tok.Type == TT_INT || tok.Type == TT_FLOAT {
 		p.advance()
-		return NumberNode{Tok: tok}
+		return res.success(NumberNode{Tok: tok})
 	} else if tok.Type == TT_LROUNDBR {
 		p.advance()
-		expr := p.expr()
+		exprRes := p.expr()
+		if exprRes.Error != "" {
+			return exprRes
+		}
 		if p.Current_Tok.Type == TT_RROUNDBR {
 			p.advance()
-			return expr
+			return res.success(exprRes.Node)
 		} else {
-			panic("Expected ')'")
+			return res.failure("Expected ')'")
 		}
 	}
-	panic("Expected int or float or (")
+	return res.failure("Expected int, float, or '('")
 }
 
-func (p *Parser) term() interface{} {
+func (p *Parser) term() *ParseResult {
 	return p.bin_op(p.factor, []TokenType{TT_MUL, TT_DIV})
 }
 
-func (p *Parser) expr() interface{} {
-	return p.bin_op(p.term, []TokenType{TT_PLUS, TT_MINUS})
+func (p *Parser) expr() *ParseResult {
+	res := p.bin_op(p.term, []TokenType{TT_PLUS, TT_MINUS})
+
+	// Check if there are multiple consecutive numbers without operators
+	if res.Error == "" && p.Current_Tok.Type != TT_EOF {
+		// If we have not reached EOF, check for invalid syntax
+		if p.Current_Tok.Type == TT_INT || p.Current_Tok.Type == TT_FLOAT {
+			return res.failure(NewInvalidSyntaxError(
+				p.Current_Tok.Pos_Start, p.Current_Tok.Pos_End,
+				"Unexpected consecutive numbers, expected operator",
+			).asString())
+		}
+	}
+	return res
 }
 
 ////////////
 
-func (p *Parser) bin_op(Func func() interface{}, ops []TokenType) interface{} {
-	left := Func()
+func (p *Parser) bin_op(funcToCall func() *ParseResult, ops []TokenType) *ParseResult {
+	res := &ParseResult{}
+	left := res.register(funcToCall())
+	if res.Error != "" {
+		return res
+	}
 
 	for p.Current_Tok.Type != TT_EOF && contains(ops, p.Current_Tok.Type) {
 		op_tok := p.Current_Tok
 		p.advance()
-		right := Func()
+		right := res.register(funcToCall())
+		if res.Error != "" {
+			return res
+		}
 		left = BinOpNode{Left_Node: left, Op_Tok: op_tok, Right_Node: right}
 	}
 
-	return left
+	return res.success(left)
 }
 
 // Utility function to check if a TokenType is in the list
@@ -337,7 +448,10 @@ func run(text string, fn string) (interface{}, error) {
 
 	// Generate AST
 	parser := NewParser(tokens)
-	ast := parser.expr()
+	res := parser.expr()
 
-	return ast, nil
+	if res.Error != "" {
+		return nil, fmt.Errorf(res.Error)
+	}
+	return res.Node, nil
 }
